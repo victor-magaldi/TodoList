f58deb02bee1c310628fb8a9188eba785526723c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-05 20:37:53 -0300

    add o componete Sobre no App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Todolist from "./components/TodoList";
 import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
 
 class App extends Component {
   render() {
