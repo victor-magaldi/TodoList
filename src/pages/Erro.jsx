52064f787730a7b15b35963dde6896767e4d3df1
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-15 19:23:48 -0300

    pegando o parametro de busca vazia

--- src/pages/Erro.jsx
+++ src/pages/Erro.jsx
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 class Erro extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { termoPesquisado: "" };
+  }
+  componentDidMount() {
+    console.log(this.props.match.params[0]);
+    const termoParam = this.props.match.params[0];
+    this.setState({ termoPesquisado: termoParam });
+    console.log(this.state);
   }
   render() {
     return (
       <div>
         <h1>Ops Pagina não encontrada</h1>
-        <h3>você está Procurando por:</h3>
+        <h3>você está Procurando por: {this.state.termoPesquisado}</h3>
         <Link to="/">voltar para Home</Link>
       </div>
     );
